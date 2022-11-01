adba9fdb30afc05f1d086e15b63e96b25c989f09
Jacopo De Gattis <jacopo.degattis@gmail.com>
2022-11-01 21:08:23 +0100

    Add line onclick to seek track progress Improve code logic

--- widget.ts
+++ widget.ts
@@ -8,15 +8,17 @@ import { WidgetAPI } from "@johnlindquist/kit/types/pro";
 // const chrome = await npm("chrome-cookies-secure");
 const spotify = await npm("spotify-node-applescript");
 
+interface RealtimeLyricsLines {    
+    startTimeMs: string,
+    words: string,
+    syllables: [],
+    endTimeMs: string
+}
+
 interface RealtimeLyrics {
     lyrics: {
         syncType: string,
-        lines: Array<{
-            startTimeMs: string,
-            words: string,
-            syllables: [],
-            endTimeMs: string
-        }>,
+        lines: Array<RealtimeLyricsLines>,
         provider: string,
         providerLyricsId: string,
         providerDisplayName: string,
@@ -31,8 +33,14 @@ interface RealtimeLyrics {
     hasVocalRemoval: boolean
 }
 
-const wgt: WidgetAPI = await widget(`
+const state = {
+    currentLyrics: null,
+    previousProcess: null,
+    isProcessLaunched: false,
+    previousTrack: null,
+}
 
+const wgt: WidgetAPI = await widget(`
     <div class="main-container">
         <header>
             <h2>{{title}} - {{artist}}</h2>
@@ -46,7 +54,7 @@ const wgt: WidgetAPI = await widget(`
                     <div class="highlighted">{{line.words}}</div>
                 </template>
                 <template v-else>
-                    <div id="unfocused-line" class="grayed-out">{{line.words}}</div>
+                    <div :key="index" :data-name="index" :data-index="index" id="unfocused-line" class="grayed-out">{{line.words}}</div>
                 </template>
             </label>
         </div>
@@ -74,17 +82,36 @@ const wgt: WidgetAPI = await widget(`
             color: #2e2e2e;
         }
 
-        // .grayed-out:hover {
-        //     cursor: pointer;
-        //     color: white;
-        // }
+        .grayed-out:hover {
+            cursor: pointer;
+            color: white;
+        }
 
         .lyrics-container > label {
             display: block;
             font-size: 1.4rem;
+            font-weight: 600;
         }
-
     </style>
+
+    <script>
+        setTimeout(() => {
+            const buttons = document.querySelectorAll("#unfocused-line");
+            
+            buttons.forEach((button) => {
+                button.addEventListener("click", (event) => {
+                    ipcRenderer.send("WIDGET_CLICK", {
+                        targetId: "jumpto",
+                        index: event.target.dataset.index,
+                        name: event.target.dataset.name,
+                        widgetId: window.widgetId,
+                    })
+                })
+            })
+
+        }, 2000)
+
+    </script>
 `, {
     width: 700,
     height: 1200,
@@ -92,7 +119,17 @@ const wgt: WidgetAPI = await widget(`
     backgroundColor: "#7C7B7C"
 })
 
-const syncLyrics = (lyrics: any, time: any) => {
+wgt.onClick((event: any) => {
+    dev({ e: event });
+    if (event.targetId === "jumpto") {
+        spotify.jumpTo(state.currentLyrics[parseInt(event.name)].startTimeMs / 1000, () => console.log("JUMPED TO"));
+    }
+})
+
+const syncLyrics: (
+    lyrics: Array<RealtimeLyricsLines>,
+    time: number
+) => number = (lyrics, time) => {
     const scores = [];
 
     lyrics.forEach((lyric: any) => {
@@ -108,14 +145,16 @@ const syncLyrics = (lyrics: any, time: any) => {
     return scores.indexOf(closest);
 }
 
-const fetchRealtimeLyrics: (trackId: string) => Promise<RealtimeLyrics> = async (trackId) => {
+const fetchRealtimeLyrics: (
+    trackId: string
+) => Promise<RealtimeLyrics> = async (trackId) => {
     const endpoint = "https://spclient.wg.spotify.com/color-lyrics/v2/track";
 
     const request = await fetch(`${endpoint}/${trackId}`, {
         method: "GET",
         headers: {
             "accept": "application/json",
-            "authorization": "Bearer BQBiJpJaFDT-XL5DCSpC_lPZRIICCIFnPYvmaYErbqTv_e-MHdg3iW3hWmV07DUOfjo49GA2vCjCNrDfziDqYLgN2ktECleWCMaZeRt_Dz16RZWf73qA3AsDMw6I0V80J4VLRINjcsjXxrovthnddbdfxWByrcCfRJNrsAUOoP6E4rgHHU6aFge9hsgiVGsj5mjxcpoj-Nx2H-uy0wuNuWNm_bTzyIy-9d2kvesyhXxPtRmLFte8Ip61TBvjjitcDg1bZBN7TpGNmYZQ0O8fRonzINE3slRXqvzrqiZOsumtl0j7_VUsHHRPNwC_czoyUyCJVJGw00XK0jmlHZrk",
+            "authorization": "Bearer BQDLjLuzEF-TZOwwhuUf8Y41jsb-CYCNb-qapZqVfQiDoW_AxpEfE3fj5UTS4J_3yD6DD3_xTiJc9kAbPWNCNWOFhY4ZgXxX7SehqbWT8wJk3db53lsrBAVmDiuTmBY3vb3SKH_m4M3uzpdFEwBaJLG66taYPmwAtomm77j2rpikTkn4OG3tKTYcxP7xkj7if95d1zaBWHGCipocI4i09s6iEAp2_b8WwkjK7XaWaTU6Ur_mefI0rRJr5VP6jpqz4Ve5mZ8f-ZQfAd8e-Oe8qRAg5D0hhlnf833kL9FCNfLLUKXch61_oYw1Ka2wwf43IDu4IE8y8oHoKlFnFuH6",
             "user-agent": "Spotify/8.7.78.373 Android/29 (Android SDK built for arm64)",
             "spotify-app-version": "8.7.78.373"
         }
@@ -137,36 +176,63 @@ const script = `
     end tell
 `
 
-const pipeCommandOutput = (command: string, callback: any) => {
+const pipeCommandOutput = (command: string, callback: (x: any) => void) => {
     const cmd = executeCustom(command);
     cmd.stdout.setEncoding("utf8");
     // cmd.stdout.on("data", (data) => wgt.setState({ position: JSON.parse(data).position }));
     // Why it works on stderr and not stdout ?
     cmd.stderr.on("data", callback);
     cmd.on("close", () => {});
+    return cmd;
 }
 
-spotify.isRunning((err: any, isRunning: boolean) => {
-    if (!isRunning) return
+const killAndRelaunch = (command: string, prevProcess: any, callback: (x: any) => void) => {
+    prevProcess.kill();
+    return pipeCommandOutput(command, callback);
+}
 
-    spotify.getTrack(async (err: any, track: any) => {
-        if (err) return
-        
-        wgt.setState({ isLoading: true })
-        const data = await fetchRealtimeLyrics(track.id.split(":").pop());
-        
-        wgt.setState({
-            title: track.name,
-            artist: track.artist,
-            isLoading: false,
-            lyrics: data.lyrics.lines,
-            position: 0
-        })
+setInterval(() => {
+    spotify.isRunning((err: any, isRunning: boolean) => {
+        if (!isRunning) return
 
-        pipeCommandOutput(`osascript -e '${script}'`, (x: any) => {
-            const currIndex = syncLyrics(data.lyrics.lines, x)
-            wgt.setState({ position: x, currentLine: currIndex })
+        spotify.getTrack(async (err: any, track: any) => {
+            if (err) return
+
+            if (state.previousTrack !== track.name) {
+                wgt.setState({ position: 0, currentLine: 0 })
+
+                wgt.setState({ isLoading: true })
+                const data = await fetchRealtimeLyrics(track.id.split(":").pop());
+                
+                if (!data) return;
+
+                if (!state.isProcessLaunched) {
+                    state.previousProcess = pipeCommandOutput(`osascript -e '${script}'`, (x: any) => {
+                        const currIndex = syncLyrics(data.lyrics.lines, x)
+                        wgt.setState({ position: x, currentLine: currIndex })
+                    })
+                } else {
+                    if (!state.previousProcess) return;
+
+                    state.previousProcess = killAndRelaunch(`osascript -e '${script}'`, state.previousProcess, (x: any) => {
+                        const currIndex = syncLyrics(data.lyrics.lines, x)
+                        wgt.setState({ position: x, currentLine: currIndex })
+                    });
+                }
+
+                wgt.setState({
+                    title: track.name,
+                    artist: track.artist,
+                    isLoading: false,
+                    lyrics: data.lyrics.lines,
+                    position: 0
+                })
+
+                state.currentLyrics = data.lyrics.lines;
+                state.isProcessLaunched = true;
+                state.previousTrack = track.name;
+            }
+
         })
     })
-})
-
+}, 1000)
